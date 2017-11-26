294db76f70b07bf9241e9a2f477649b1b7ccd324
Nikita CryptoManiac Sivakov <cryptomaniac.512@gmail.com>
2017-11-26 23:58:52 +1000

    Initial project structure

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    author="Nikita Sivakov",
+    author_email="cryptomaniac.512@gmail.com",
+    description="Easy and fast project switching in your shell!",
+    keywords="project shell management",
+    license="MIT",
+    long_description_markdown_filename='README.md',
+    name="goto-project",
+    packages=["goto_project"],
+    setup_requires=['setuptools-markdown'],
+    url="https://github.com/cryptomaniac512/goto-project",
+    version="0.0.1",
+)
