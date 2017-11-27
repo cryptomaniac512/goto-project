c9ddb839bde93dd9e18b20cf88b51b1c310bc94a
Nikita CryptoManiac Sivakov <cryptomaniac.512@gmail.com>
2017-11-27 19:10:40 +1000

    Implement project opening

--- goto_project/manager.py
+++ goto_project/manager.py
@@ -1,9 +1,13 @@
+import subprocess
 import typing
 
 from goto_project.config_tools import load_config
+from goto_project.shell_tools import ExpressionConstructor
 
 
 class Manager:
+
+    constructor = ExpressionConstructor
 
     def __init__(self):
         self.config = load_config()
@@ -13,3 +17,8 @@ class Manager:
 
     def project_config(self, project_name: str) -> dict:
         return self.config[project_name] or {}
+
+    def open_project(self, project_name: str):
+        call_args = self.constructor.construct(
+            project_name, self.project_config(project_name))
+        subprocess.call(call_args)
